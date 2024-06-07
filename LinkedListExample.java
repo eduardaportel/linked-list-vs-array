d39f5918184ef9df422ca2f7226bf0699dcd0565
Eduarda Portel <eduarda.portel4@gmail.com>
2024-06-06 22:05:34 -0300

    treating the linked list as stack and queue

--- LinkedListExample.java
+++ LinkedListExample.java
@@ -4,22 +4,28 @@ public class LinkedListExample{
 
         // name of the LinkedList = linked list
         LinkedList<String> linkedList = new LinkedList<String>();
+        // we can treat or Linked List as a queue or a stack
 
         //  stack = LIFO; ex = stack of CD's
-
+        // add = push(); remove = pop()
         linkedList.push("A");
         linkedList.push("B");
         linkedList.push("C");
         linkedList.push("D");
         linkedList.push("F");
+        linkedList.pop();
         System.out.println(linkedList);
-        // F D C B A
+        // D C B A
 
-        // queue
-        // linkedList.offer("A");
-        // linkedList.offer("B");
-        // linkedList.offer("C");
-        // linkedList.offer("D");
-        // linkedList.offer("F");
+        // queue = FIFO; ex = queue of a market
+        // add = offer(); remove = poll();
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        linkedList.poll();
+        System.out.println(linkedList);
+        // BCDF
     }
 }
